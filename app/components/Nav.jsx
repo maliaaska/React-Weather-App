f427ca53ae56b185d0f17b7fdc0bf305b596befb
Arkadiusz <asmilewski@gmail.com>
2017-10-03 13:59:04 +0200

    first commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Nav = React.createClass({
+    render: function (){
+        return (
+            <h1>
+                Navigation Component
+            </h1>
+        );
+    }
+});
+
+module.exports = Nav;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
-// This file export an object and this object
-// lets you configure what webpack does
+// This file export an object and this object lets you configure what webpack
+// does
 
-
- // 1. entry property tells webpack where it should start proccessing your code
- // 2. we telling babel-loader to take our files parse them through react
- // get the output and run them through es2015 as well
- module.exports = {
+// 1. entry property tells webpack where it should start proccessing your code
+// 2. we telling babel-loader to take our files parse them through react get the
+// output and run them through es2015 as well
+module.exports = {
     entry: './app/app.jsx',
     output: {
         path: __dirname,
@@ -14,6 +13,8 @@
     resolve: {
         root: __dirname,
         alias: {
+            Main: 'app/components/Main.jsx',
+            Nav: 'app/components/Nav.jsx'
         },
         extensions: ['', '.js', '.jsx']
     },
@@ -22,7 +23,7 @@
             {
                 loader: 'babel-loader',
                 query: {
-                    presets: ['react', 'es2015', 'stage-0']
+                    presets: ['react', 'es2015']
                 },
                 test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/
