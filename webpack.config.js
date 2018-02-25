5087579d26c8a35be4c47ef36222b36edd4e1213
Arkadiusz <asmilewski@gmail.com>
2018-02-25 09:20:30 +0100

    merging before with origin master

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,7 @@
-// This file export an object and this object lets you configure what webpack
-// does
+const path = require('path');
 
-// 1. entry property tells webpack where it should start proccessing your code
-// 2. we telling babel-loader to take our files parse them through react get the
-// output and run them through es2015 as well
-module.exports = {
+module.exports = (env) => {
+  return {
     entry: './app/app.jsx',
     output: {
         path: __dirname,
@@ -38,4 +35,5 @@ module.exports = {
 
     },
     devtool: 'cheap-module-eval-source-map'
+}
 };
